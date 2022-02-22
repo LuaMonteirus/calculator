0519ebf3791d4be943d68bf597e2267053cc7e21
Luan Gabriel <luangflobo@gmail.com>
2022-02-21 23:42:47 -0300

    refactor: use switch, organize math operations

--- src/script.js
+++ src/script.js
@@ -11,33 +11,27 @@ const message = `Choose a number:
 // --------------- Math Operations ------------------ //
 
 const addition = (a, b) => {
-    result = a + b;
-    alert(`${a} plus ${b} is ${result}`);
+    return a + b;
     };
 
 const subtraction = (a, b) => {
-    result = a - b
-    alert(`${a} minus ${b} is ${result}`);
+    return a - b
     };
 
 const multiplication = (a, b) => {
-    result = a * b;
-    alert(`${a} multiplied by ${b} is ${result}`);
+    return a * b;
     };
 
 const division = (a, b) => {
-    result = a / b;
-    alert(`${a} divided by ${b} is ${result}`);
+    return a / b;
     };
 
 const remaider = (a, b) => {
-    result = a % b;
-    alert(`The remaider of ${a} divided by ${b} is ${result}`);
+    return a % b;
     };
 
 const exponentiation = (a, b) => {
-    result = a ** b;
-    alert(`${a} raised to the ${b}th power is ${result}`);
+    return a ** b;
 };
 
 // ---------------- New Operation ----------------- //
@@ -58,7 +52,7 @@ const newOp = () => {
 // -------------- Calculator ------------------ //
 
 const cal = () => {
-    const operation = prompt(message);
+    const operation = Number(prompt(message));
     if (isNaN(operation) || operation >= 7 || operation <= 0) {
         alert(`Error - Put a valid option.`);
         cal();
@@ -72,28 +66,38 @@ const cal = () => {
         } else {
     
 // --------------- Conditional's operation ----------------- //
-    
-    if (operation == 1) {
-        addition(a, b);
-        newOp();
-    } else if (operation == 2) {
-        subtraction(a, b);
-        newOp();
-    } else if (operation == 3) {
-        multiplication(a, b);
-        newOp();
-    } else if (operation == 4) {
-        division(a, b);
-        newOp();
-    } else if (operation == 5) {
-        remaider(a, b);
-        newOp();
-    } else if (operation == 6) {
-        exponentiation(a, b);
-        newOp();
-    }
+
+    switch (operation) {
+        case 1:
+            addition(a, b)
+            alert(`${a} plus ${b} is ${addition(a, b)}`);
+            break;
+        case 2:
+            subtraction(a, b)
+            alert(`${a} minus ${b} is ${subtraction(a, b)}`);
+            break;
+        case 3: 
+            multiplication (a, b)
+            alert(`${a} multiplied by ${b} is ${multiplication(a, b)}`);
+            break;
+        case 4:
+            division (a, b)
+            alert(`${a} divided by ${b} is ${division(a, b)}`);
+            break;
+        case 5: 
+            remaider (a, b)
+            alert(`The remaider of ${a} divided by ${b} is ${remaider(a, b)}`);
+            break;
+        case 6:
+            exponentiation (a, b)
+            alert(`${a} raised to the ${b}th power is ${exponentiation(a, b)}`);
+            break;
+        default:
+            alert('a')
+        }
+    newOp()
    }
   }
 }
 
-cal() 
+cal()
